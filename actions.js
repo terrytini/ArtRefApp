6ff9dcfa3d5a5b286ebea7a021079dcd3d6f3551
Jennifer Bramson <jib41@pitt.edu>
2017-01-21 09:11:36 -0500

    added js file

---
+++ actions.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var xml = new XMLHttpRequest();
+  xhttp.onreadystatechange = functon() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+
+});
+
+myFunction(xml) {
+  searchtype=image
+  start=0
+  &num=5
+  &q=monkey
+  &as_filetype=png
+  &client=google-csbe
+  &output=xml_no_dtd
+  &cx=00255077836266642015:u-scht7a-8i
+}
